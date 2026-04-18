f6387ead5716ec9e1514dc578c060ea0b8bd3e49
Ihor A <ihor.aleshchenko10@gmail.com>
2026-04-18 17:40:47 +0200

    wip: building imax structure

--- pages/imax.page.ts
+++ pages/imax.page.ts
@@ -1,8 +1,33 @@
 import { Locator, Page, expect } from "@playwright/test";
 
 export class ImaxPage {
+  readonly getToKnow: Locator;
+  readonly imaxHeading: Locator;
+  readonly sadybaLink: Locator;
+  readonly poznanPlazaLink: Locator;
+  readonly punkt44Link: Locator;
+  readonly manufakturaLink: Locator;
+  readonly wroclaviaLink: Locator;
+  readonly zakopiankaLink: Locator;
+  readonly moreAboutImax: Locator;
 
-    constructor(private page: Page){
+  constructor(page: Page) {
+    this.getToKnow = page.getByRole('tab', { name: 'Poznaj' });
+    this.imaxHeading = page.getByRole('heading', { name: 'IMAX', level: 4 });
+    this.sadybaLink = page.getByRole('link', { name: /Sadyba/ });
+    this.poznanPlazaLink = page.getByRole('link', { name: /Poznań Plaza/ });
+    this.punkt44Link = page.getByRole('link', { name: /Punkt 44/ });
+    this.manufakturaLink = page.getByRole('link', { name: /Manufaktura/ });
+    this.wroclaviaLink = page.getByRole('link', { name: /Wroclavia/ });
+    this.zakopiankaLink = page.getByRole('link', { name: /Zakopianka/ });
+    this.moreAboutImax = page.getByRole('tab', { name: /więcej o imax/i });
+  }
 
+  async assertLinksHref(links: { locator: Locator; expectedHref: string | RegExp }[]) {
+    for (const { locator, expectedHref } of links) {
+      await expect(locator).toBeVisible();
+      const href = await locator.getAttribute('href');
+      expect(href).toMatch(expectedHref);
     }
+  }
 }
--- tests/regression/imax.spec.ts
+++ tests/regression/imax.spec.ts
@@ -1,31 +1,53 @@
 import { test, expect } from "../../fixtures/errorListener.fixture";
 import { Header } from "../../pages/components/header.component";
+import { ImaxPage } from "../../pages/imax.page";
 
-test.describe ("@regression @imax Imax", () =>{
-    test.describe.configure({ retries: 1 });
-      // Cloudflare may intercept on first load
-      let header: Header;
+test.describe("@regression @imax Imax", () => {
+  test.describe.configure({ retries: 1 });
+  // Cloudflare may intercept on first load
+  let header: Header;
+  let imax: ImaxPage;
 
-    test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("./");
     header = new Header(page);
-     });
-
-     test("Test", async () => {
-    await header.switchToEnglish();
-    await expect(header.whatsOnLink).toBeVisible();
-
-    await header.switchToPolish();
-    await expect(header.repertuarLink).toBeVisible();
+    imax = new ImaxPage(page);
+    await header.imaxLink.click();
   });
 
-  test("Test 1", async () => {
-    await header.switchToEnglish();
-    await expect(header.whatsOnLink).toBeVisible();
-
-    await header.switchToPolish();
-    await expect(header.repertuarLink).toBeVisible();
+  test("should open IMAX page", async ({ page }) => {
+    await expect(page).toHaveURL(/\/imax/);
+    await expect(imax.getToKnow).toBeVisible();
   });
-}
 
-)
+
+  test("should display IMAX section heading", async () => {
+    await expect(imax.imaxHeading).toBeVisible();
+  });
+
+  test("should display all cinema locations", async () => {
+    await expect(imax.sadybaLink).toBeVisible();
+    await expect(imax.poznanPlazaLink).toBeVisible();
+    await expect(imax.punkt44Link).toBeVisible();
+    await expect(imax.manufakturaLink).toBeVisible();
+    await expect(imax.wroclaviaLink).toBeVisible();
+    await expect(imax.zakopiankaLink).toBeVisible();
+  });
+
+  test("should have correct hrefs for cinema locations", async () => {
+    await imax.assertLinksHref([
+      { locator: imax.sadybaLink,      expectedHref: /\/kina\/sadyba/ },
+      { locator: imax.poznanPlazaLink, expectedHref: /\/kina\/poznanplaza/ },
+      { locator: imax.punkt44Link,     expectedHref: /\/kina\/punkt44/ },
+      { locator: imax.manufakturaLink, expectedHref: /\/kina\/manufaktura/ },
+      { locator: imax.wroclaviaLink,   expectedHref: /\/kina\/wroclavia/ },
+      { locator: imax.zakopiankaLink,  expectedHref: /\/kina\/zakopianka/ },
+    ]);
+  });
+
+  test("should open more about IMAX page", async ({ page }) => {
+    await expect(page).toHaveURL(/\/imax/);
+    await expect(imax.moreAboutImax).toBeVisible();
+  });
+
+});
