a73ff84a61eadeafb744967e80f716b399d27512
Ihor A <ihor.aleshchenko10@gmail.com>
2026-04-20 17:17:58 +0200

    Add IMAX structure

--- pages/imax.page.ts
+++ pages/imax.page.ts
@@ -10,8 +10,19 @@ export class ImaxPage {
   readonly wroclaviaLink: Locator;
   readonly zakopiankaLink: Locator;
   readonly moreAboutImax: Locator;
+  readonly filmAdventureHeading: Locator;
+  readonly imaxWithLaserHeading: Locator;
+  readonly videoSlider: Locator;
+  readonly activeVideoThumb: Locator;
+  readonly selectCinemaHeading: Locator;
+  readonly katowiceButton: Locator;
+  readonly krakowButton: Locator;
+  readonly lodzButton: Locator;
+  readonly poznanButton: Locator;
+  readonly warszawaButton: Locator;
+  readonly wroclawButton: Locator;
 
-  constructor(page: Page) {
+  constructor(private readonly page: Page) {
     this.getToKnow = page.getByRole('tab', { name: 'Poznaj' });
     this.imaxHeading = page.getByRole('heading', { name: 'IMAX', level: 4 });
     this.sadybaLink = page.getByRole('link', { name: /Sadyba/ });
@@ -21,6 +32,21 @@ export class ImaxPage {
     this.wroclaviaLink = page.getByRole('link', { name: /Wroclavia/ });
     this.zakopiankaLink = page.getByRole('link', { name: /Zakopianka/ });
     this.moreAboutImax = page.getByRole('tab', { name: /więcej o imax/i });
+    this.filmAdventureHeading = page.getByText(/FILMOWA PRZYGODA TWOJEGO ŻYCIA/i);
+    this.imaxWithLaserHeading = page.getByText(/IMAX DMR/i);
+    this.videoSlider = page.locator('[data-slick="slick-video"]');
+    this.activeVideoThumb = page.locator('.slick-video-thumb[aria-pressed="true"]');
+    this.selectCinemaHeading = page.getByText(/Wybierz kino, aby zobaczyć dostępne filmy w formacie /i);
+    this.katowiceButton = page.locator('[data-automation-id="chooseExperienceCinemaButton"]', { hasText: /Katowice/i });
+    this.krakowButton = page.locator('[data-automation-id="chooseExperienceCinemaButton"]', { hasText: /Kraków/i });
+    this.lodzButton = page.locator('[data-automation-id="chooseExperienceCinemaButton"]', { hasText: /Łódź/i });
+    this.poznanButton = page.locator('[data-automation-id="chooseExperienceCinemaButton"]', { hasText: /Poznań/i });
+    this.warszawaButton = page.locator('[data-automation-id="chooseExperienceCinemaButton"]', { hasText: /Warszawa/i });
+    this.wroclawButton = page.locator('[data-automation-id="chooseExperienceCinemaButton"]', { hasText: /Wrocław/i });
+  }
+
+  getScheduleHeading(cinemaName: string): Locator {
+    return this.page.getByRole('heading', { name: new RegExp(cinemaName, 'i'), level: 2 });
   }
 
   async assertLinksHref(links: { locator: Locator; expectedHref: string | RegExp }[]) {
--- tests/regression/imax.spec.ts
+++ tests/regression/imax.spec.ts
@@ -20,34 +20,54 @@ test.describe("@regression @imax Imax", () => {
     await expect(imax.getToKnow).toBeVisible();
   });
 
-
   test("should display IMAX section heading", async () => {
     await expect(imax.imaxHeading).toBeVisible();
   });
 
-  test("should display all cinema locations", async () => {
-    await expect(imax.sadybaLink).toBeVisible();
-    await expect(imax.poznanPlazaLink).toBeVisible();
-    await expect(imax.punkt44Link).toBeVisible();
-    await expect(imax.manufakturaLink).toBeVisible();
-    await expect(imax.wroclaviaLink).toBeVisible();
-    await expect(imax.zakopiankaLink).toBeVisible();
-  });
-
   test("should have correct hrefs for cinema locations", async () => {
     await imax.assertLinksHref([
-      { locator: imax.sadybaLink,      expectedHref: /\/kina\/sadyba/ },
+      { locator: imax.sadybaLink, expectedHref: /\/kina\/sadyba/ },
       { locator: imax.poznanPlazaLink, expectedHref: /\/kina\/poznanplaza/ },
-      { locator: imax.punkt44Link,     expectedHref: /\/kina\/punkt44/ },
+      { locator: imax.punkt44Link, expectedHref: /\/kina\/punkt44/ },
       { locator: imax.manufakturaLink, expectedHref: /\/kina\/manufaktura/ },
-      { locator: imax.wroclaviaLink,   expectedHref: /\/kina\/wroclavia/ },
-      { locator: imax.zakopiankaLink,  expectedHref: /\/kina\/zakopianka/ },
+      { locator: imax.wroclaviaLink, expectedHref: /\/kina\/wroclavia/ },
+      { locator: imax.zakopiankaLink, expectedHref: /\/kina\/zakopianka/ },
     ]);
   });
 
-  test("should open more about IMAX page", async ({ page }) => {
-    await expect(page).toHaveURL(/\/imax/);
-    await expect(imax.moreAboutImax).toBeVisible();
+  test("should switch between tabs correctly", async () => {
+    await imax.moreAboutImax.click();
+    await expect(imax.moreAboutImax).toHaveAttribute("aria-selected", "true");
+    await imax.getToKnow.click();
+    await expect(imax.getToKnow).toHaveAttribute("aria-selected", "true");
+    await expect(imax.moreAboutImax).toHaveAttribute("aria-selected", "false");
   });
 
+  test("should display Experience IMAX content headings", async () => {
+    await imax.moreAboutImax.click();
+    await expect(imax.filmAdventureHeading).toBeVisible();
+    await expect(imax.imaxWithLaserHeading).toBeVisible();
+  });
+
+  test("should display video slider on Experience tab", async () => {
+    await imax.getToKnow.click();
+
+    await expect(imax.videoSlider).toBeVisible();
+    await expect(imax.activeVideoThumb).toBeVisible();
+  });
+
+  test("should load schedule after selecting a cinema", async () => {
+    await imax.katowiceButton.click();
+    await expect(imax.getScheduleHeading("Katowice")).toBeVisible();
+  });
+
+  test("should display all IMAX cinemas in Poland", async () => {
+    await expect(imax.selectCinemaHeading).toBeVisible();
+    await expect(imax.katowiceButton).toBeVisible();
+    await expect(imax.krakowButton).toBeVisible();
+    await expect(imax.lodzButton).toBeVisible();
+    await expect(imax.poznanButton).toBeVisible();
+    await expect(imax.warszawaButton).toBeVisible();
+    await expect(imax.wroclawButton).toBeVisible();
+  });
 });
