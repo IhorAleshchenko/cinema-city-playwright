702d4b749207068257adaa7b864f81198f5e93b9
Ihor A <ihor.aleshchenko10@gmail.com>
2026-04-25 19:20:09 +0200

    Add offers skeleton

--- pages/offers.page.ts
+++ pages/offers.page.ts
@@ -1,1 +1,8 @@
 import { Locator, Page, expect } from "@playwright/test";
+
+export class Offers {
+    readonly offersHeading: Locator;
+    constructor(page:Page) {
+      this.offersHeading = page.getByRole('heading', { name: 'OFERTY', level: 1 })
+    }
+}
--- tests/regression/offers.spec.ts
+++ tests/regression/offers.spec.ts
@@ -1,1 +1,25 @@
-import test, { expect } from "@playwright/test";
+import { test, expect } from "../../fixtures/errorListener.fixture";
+import { Header } from "../../pages/components/header.component";
+import { Offers } from "../../pages/offers.page";
+
+// test.describe("@regression @offers Offers", () => {
+//   let header: Header;
+//   let offers: Offers;
+
+// })
+
+test.describe("@regression @offers Offers", () => {
+  let header: Header;
+  let offers: Offers;
+
+  test.beforeEach(async({ page }) => {
+    await page.goto("./")
+    header = new Header(page);
+    offers = new Offers(page);
+    await header.offersLink.click();
+  })
+
+  test("Should open Offers link", async() => {
+    await expect(offers.offersHeading).toBeVisible();
+  })
+})
