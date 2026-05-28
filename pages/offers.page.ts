f1274b5f7f714207750ef4dc8389e7972926508b
Ihor A <ihor.aleshchenko10@gmail.com>
2026-05-28 22:37:39 +0200

    wip: add offer page voucher flow

--- pages/offers.page.ts
+++ pages/offers.page.ts
@@ -2,7 +2,39 @@ import { Locator, Page, expect } from "@playwright/test";
 
 export class Offers {
     readonly offersHeading: Locator;
+    readonly checkVoucher: Locator;
+    readonly orderVouchersButton: Locator;
+    readonly categoryHeading: Locator;
+    // Voucher form 
+    readonly voucherGiftCardsOption: Locator;
+    readonly cartUnlimited: Locator;
+      readonly reasonField: Locator;
+  readonly reasonDropdown: Locator;
+  readonly contractTerminationOption: Locator;
+
+
     constructor(page:Page) {
-      this.offersHeading = page.getByRole('heading', { name: 'OFERTY', level: 1 })
+      this.offersHeading = page.getByRole('heading', { name: 'OFERTY', level: 1 });
+      this.checkVoucher = page.getByRole('button', { name: 'SPRAWDŹ'});
+      this.orderVouchersButton = page.getByRole('button', { name: 'ZAMÓW VOUCHERY' });
+      this.categoryHeading = page.getByRole('heading', { name: 'Wybierz kategorię' });
+      // Voucher form 
+      this.voucherGiftCardsOption = page.getByRole("option", {name: "Vouchery i Karty Podarunkowe"});
+      this. cartUnlimited = page.getByRole("option", {name: "Karta Unlimited"});
+      this.reasonField = page.locator(".form-group").filter({has: page.getByLabel("Powód zgłoszenia")});
+      this.reasonDropdown = this.reasonField.locator('[role="combobox"]');
+      this.contractTerminationOption = this.reasonField.getByRole("option", {name: "Rozwiązanie umowy"});
+      
+         
+       
+
     }
+
+     async openVoucherCategories() {
+    await this.checkVoucher.click();
+    await this.orderVouchersButton.click();
+    await expect(this.categoryHeading).toBeVisible();
+  }
+
+ 
 }
--- tests/regression/offers.spec.ts
+++ tests/regression/offers.spec.ts
@@ -2,11 +2,11 @@ import { test, expect } from "../../fixtures/errorListener.fixture";
 import { Header } from "../../pages/components/header.component";
 import { Offers } from "../../pages/offers.page";
 
-// test.describe("@regression @offers Offers", () => {
-//   let header: Header;
-//   let offers: Offers;
+test.describe("@regression @offers Offers", () => {
+  let header: Header;
+  let offers: Offers;
 
-// })
+})
 
 test.describe("@regression @offers Offers", () => {
   let header: Header;
@@ -16,10 +16,29 @@ test.describe("@regression @offers Offers", () => {
     await page.goto("./")
     header = new Header(page);
     offers = new Offers(page);
-    await header.offersLink.click();
+   await header.offersLink.click();
   })
 
   test("Should open Offers link", async() => {
     await expect(offers.offersHeading).toBeVisible();
+    await offers.checkVoucher.click();
+
   })
+  test("Should open Voucher link", async() => {
+    //Act
+    await offers.checkVoucher.click();
+    await offers.orderVouchersButton.click();
+    await expect(offers.categoryHeading).toBeVisible();
+  })
+
+  test("Should fill up Voucher form", async() => {
+    //Act
+    await offers.openVoucherCategories()
+   await offers.voucherGiftCardsOption.click()
+   await offers.cartUnlimited.click()
+   await offers.reasonDropdown.click()
+   await offers.contractTerminationOption.click()
+
+  })
+
 })
